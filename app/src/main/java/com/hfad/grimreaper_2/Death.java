0925b8baab849653bb9f252afc3f6775eb84d1c0
TheBullseye23 <karan87108@gmail.com>
2019-07-21 01:04:36 +0530

    Added code for changing colour of background

--- app/src/main/java/com/hfad/grimreaper_2/Death.java
+++ app/src/main/java/com/hfad/grimreaper_2/Death.java
@@ -1,6 +1,8 @@
 package com.hfad.grimreaper_2;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ public class Death extends AppCompatActivity {
 
     EditText d1;
     Button d2;
+    ConstraintLayout d3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class Death extends AppCompatActivity {
 
         d1=findViewById(R.id.Entry);
         d2=findViewById(R.id.guess);
+        d3=findViewById(R.id.deathlayout);
         d2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,12 +39,15 @@ public class Death extends AppCompatActivity {
                 String q = intent.getStringExtra("ANSWER");
                 if ((Integer.valueOf(q) == Integer.valueOf(x))&&clickcount<=Integer.valueOf(k))                      //verifying the guess (equal)
                  {Toast.makeText(getApplicationContext(), "Bang On!", Toast.LENGTH_SHORT).show();
+                   d3.setBackgroundColor(Color.rgb(255,255,255));
                     onBackPressed();}
                 if ((Integer.valueOf(q) < Integer.valueOf(x))&&clickcount<Integer.valueOf(k))
                 {Toast.makeText(getApplicationContext(), "Oops,the person lived longer than destined", Toast.LENGTH_SHORT).show();
+                   d3.setBackgroundColor(Color.argb( ((Integer.valueOf(x)-Integer.valueOf(q))*2),255,0,0));
                   onBackPressed();}
                 if ((Integer.valueOf(q) > Integer.valueOf(x))&&clickcount<Integer.valueOf(k))
                 {Toast.makeText(getApplicationContext(), "Oops,you reaped the person's soul a tad bit too soon", Toast.LENGTH_SHORT).show();
+                    d3.setBackgroundColor(Color.argb(((Integer.valueOf(q)-Integer.valueOf(x))*2),0,0,255));
                   onBackPressed();}
                 if ((Integer.valueOf(q) > Integer.valueOf(x))&&Integer.valueOf(k)<=clickcount)
                 {Toast.makeText(getApplicationContext(), "You've exceeded your chances,Configure to try again!", Toast.LENGTH_SHORT).show();
